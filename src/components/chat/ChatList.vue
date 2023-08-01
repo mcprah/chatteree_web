bb83633d5bf17c515f19b6a309148641dd6f626f
Mark Amoah <mcprah@gmail.com>
2023-08-01 11:05:41 +0000

    refactor: add first-time chat screen

--- src/components/chat/ChatList.vue
+++ src/components/chat/ChatList.vue
@@ -1,15 +1,100 @@
 <template>
-  <div class="">
-    <p>Chat List</p>
+  <div class="container-fluid p-3 h-100" id="chatList">
+    <div class="row" id="chatListHeader">
+      <div class="col-md-12 d-flex align-items-center justify-content-between">
+        <div class="d-flex align-items-center" id="user">
+          <div class="profile-pic rounded-pill"></div>
+          <div class="username ms-3">Kojo Trip</div>
+        </div>
+        <div class="btn-new-chart">
+          <CButton
+            class="btn p-0 btn-icon text-accent"
+            variant="primary"
+            id="btnNewChat"
+            @click="() => {}"
+          >
+            <EditIcon variant="primary" />
+          </CButton>
+        </div>
+      </div>
+    </div>
+    <div class="row my-4">
+      <div class="col-md-12">
+        <CTextInput
+          class="w-100"
+          v-model="searchQuery"
+          :placeholder="placeholder"
+          :hasError="false"
+          ><template v-slot:prefix>
+            <img
+              src="@/assets/icons/icon/interfaces/search.svg"
+              alt=""
+            /> </template
+        ></CTextInput>
+      </div>
+    </div>
+    <div class="row flex-grow-1 overflow-y-scrol">
+      <div
+        class="col-md-12 d-flex flex-column justify-content-center align-items-center"
+      >
+        <div
+          class="d-flex flex-column justify-content-center align-items-center text-center mb-5"
+          v-if="chatlist.length == 0"
+        >
+          <img src="@/assets/icons/icon/interfaces/chat.svg" alt="" />
+          <p class="mt-3 px-4 text-light">
+            Your Chatteree conversations live here
+          </p>
+        </div>
+        <div class="" v-else>
+          <p v-for="(chat, idx) in chatlist" :key="idx">Chat list view</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import CButton from "../CButton.vue";
+import EditIcon from "../icon/EditIcon.vue";
+import CTextInput from "../CTextInput.vue";
 
-}
+export default {
+  name: "chat-list",
+  components: { CButton, EditIcon, CTextInput },
+  data() {
+    return {
+      searchQuery: "",
+      chatlist: [],
+    };
+  },
+  computed: {
+    placeholder() {
+      if (this.searchQuery == "") {
+        return "Search for chatter or message";
+      }
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#chatList {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  #chatListHeader {
+    #user {
+      cursor: pointer;
+      .profile-pic {
+        width: 48px;
+        height: 48px;
+        background: $color-dark;
+      }
+      .username {
+        font-weight: 500;
+      }
+    }
+  }
+}
 </style>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -2,108 +2,70 @@
   <div class="container-fluid">
     <div class="row vh-100">
       <div class="col-lg-3">
-        <div class="container-fluid p-3 h-100" id="chatList">
-          <div class="row" id="chatListHeader">
-            <div
-              class="col-md-12 d-flex align-items-center justify-content-between"
-            >
-              <div class="d-flex align-items-center" id="user">
-                <div class="profile-pic rounded-pill"></div>
-                <div class="username ms-3">Kojo Trip</div>
-              </div>
-              <div class="btn-new-chart">
-                <CButton
-                  class="p-0 btn-icon text-accent"
-                  variant="primary"
-                  id="btnNewChat"
-                  @click="() => {}"
-                >
-                  <EditIcon variant="primary" />
-                </CButton>
-              </div>
+        <ChatList />
+      </div>
+      <div class="col-lg-9">
+        <div class="container-fluid p-3 pt-5 h-100" id="chatConversation">
+          <div class="row mt-5">
+            <div class="col-md-12 d-flex flex-column">
+              <h3 class="h5 mb-0">Welcome</h3>
+              <h1 class="h1 mb-2">Kojo Trip!</h1>
             </div>
           </div>
-          <div class="row my-4">
-            <div class="col-md-12">
-              <CTextInput
-                class="w-100"
-                v-model="searchQuery"
-                :placeholder="placeholder"
-                :hasError="false"
-                ><template v-slot:prefix>
-                  <img
-                    src="@/assets/icons/icon/interfaces/search.svg"
-                    alt=""
-                  /> </template
-              ></CTextInput>
-            </div>
-          </div>
-          <div class="row flex-grow-1 overflow-y-scrol">
+          <div class="row flex-grow-1">
             <div
-              class="col-md-12 d-flex flex-column justify-content-center align-items-center"
+              class="col-md-12 d-flex flex-column justify-content-center align-items-center pb-5"
             >
-              <div
-                class="d-flex flex-column justify-content-center align-items-center text-center mb-5"
-                v-if="chatlist.length == 0"
+              <img
+                src="@/assets/images/first-time-welcome.png"
+                width="326"
+                alt=""
+              />
+              <p class="mt-3 px-2 text-light">
+                We hear say your mouth die hmmm
+              </p>
+
+              <CButton
+                class=""
+                variant="primary"
+                id="btnNewChat"
+                @click="() => {}"
               >
-                <img src="@/assets/icons/icon/interfaces/chat.svg" alt="" />
-                <p class="mt-3 px-4 text-light" >Your Chatteree conversations live here</p>
-              </div>
-              <div class="" v-else>
-                <p v-for="(chat, idx) in chatlist" :key="idx">Chat list view</p>
-              </div>
+                <template v-slot:left-icon>
+                  <EditIcon variant="primary" class="me-2" />
+                </template>
+                Start chatting
+              </CButton>
             </div>
           </div>
         </div>
-      </div>
-      <div class="col-lg-9">
-        <div class="p-3" id="chatConversation">right</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ChatList from "../components/chat/ChatList.vue";
 import CButton from "../components/CButton.vue";
 import EditIcon from "../components/icon/EditIcon.vue";
-import CTextInput from "../components/CTextInput.vue";
 
 export default {
   name: "chat-page",
-  components: { CButton, EditIcon, CTextInput },
+  components: { ChatList, CButton, EditIcon },
   data() {
     return {
       searchQuery: "",
       chatlist: [],
     };
   },
-  computed: {
-    placeholder() {
-      if (this.searchQuery == "") {
-        return "Search for chatter or message";
-      }
-    },
-  },
+  computed: {},
 };
 </script>
 
 <style lang="scss" scoped>
-#chatList {
+#chatConversation {
   display: flex;
   flex-direction: column;
   max-height: 100vh;
-  #chatListHeader {
-    #user {
-      cursor: pointer;
-      .profile-pic {
-        width: 48px;
-        height: 48px;
-        background: $color-dark;
-      }
-      .username {
-        font-weight: 500;
-      }
-    }
-  }
 }
 </style>
